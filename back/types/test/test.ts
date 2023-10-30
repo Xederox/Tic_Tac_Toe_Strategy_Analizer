dc9fa3aeea6f644e05c5fdb52108d145fbf60269
Xederox <xederox@gmail.com>
2023-10-30 13:44:03 +0100

    Added Stage0, Stage0Cell and styles. Styles not working though.

--- back/types/test/test.ts
+++
@@ -1,3 +0,0 @@
-export interface TestFace {
-    x:number;
-}
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css';
-import {TestFace} from "types";
-import {Stage0} from './components/stage0/Stage0';
+import {Stage0} from './components/Stage0/Stage0';
 
 function App() {
-  const asdf:TestFace = {
-    x: 123,
-  };
 
   return (
-      <Stage0/>
+      <Stage0 coords={0} currPlayer={"O"}/>
   );
 }
 
