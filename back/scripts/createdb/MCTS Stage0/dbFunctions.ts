f24ff546c7fdd89a74ffeec72a8bddd391995214
Xederox <xederox@gmail.com>
2024-01-10 09:47:51 +0100

    Monte Carlo Tree Search for Stage 0 now correctly follows the algorithm.

--- back/scripts/createdb/MCTS Stage0/dbFunctions.ts
+++ back/scripts/createdb/MCTS Stage0/dbFunctions.ts
@@ -10,6 +10,14 @@ export const insertIntoDB = async (data: dbMonteRecord): Promise<void> => {
   });
 };
 
+export const updateDB = async (data: dbMonteRecord): Promise<void> => {
+  await pool.execute("UPDATE `montecarlotest2` SET id=:id, value=:value, total=:total WHERE id=:id", {
+    id: data.id,
+    value: data.value,
+    total: data.total,
+  });
+};
+
 export const selectFromDB = async (id: string): Promise<dbMonteRecord> => {
   const [results] = await pool.execute("SELECT * FROM `montecarlotest2` WHERE id=:id", {id: id}) as [dbMonteRecord[], FieldPacket[]];
   return {
@@ -17,17 +25,4 @@ export const selectFromDB = async (id: string): Promise<dbMonteRecord> => {
     value: results[0].value,
     total: results[0].total,
   };
-};
-
-export const selectValueFromDB = async (id: string): Promise<number> => {
-  const [results] = await pool.execute("SELECT * FROM `montecarlotest2` WHERE id=:id", {id: id}) as [dbMonteRecord[], FieldPacket[]];
-  return results[0].value;
-};
-
-export const updateDB = async (data: dbMonteRecord): Promise<void> => {
-  await pool.execute("UPDATE `montecarlotest2` SET id=:id, value=:value, total=:total WHERE id=:id", {
-    id: data.id,
-    value: data.value,
-    total: data.total,
-  });
 };
